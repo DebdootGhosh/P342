34ef8bf787cf588209582b51fa8daafdf1195c16
DebdootGhosh <42205626+DebdootGhosh@users.noreply.github.com>
2020-08-09 12:17:53 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,25 @@
+/*Q1*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+
+{ int i,n,sum;
+
+    printf("enter the value of n: ");
+    scanf("%d",&n);
+    sum=0;
+
+ if(n<=0)
+    printf("enter a positive whole number");
+ else{
+
+    for(i=1;i<=n;i++)
+        sum= sum+ i;
+
+
+    printf("Sum of the series is: %d\n",sum);}
+
+    return 0;
+}
+
