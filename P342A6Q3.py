69d9003c2901de6123f290712153f5a25908a78f
DebdootGhosh <42205626+DebdootGhosh@users.noreply.github.com>
2020-11-01 15:44:45 +0530

    Update P342A6Q3.py

--- P342A6Q3.py
+++ P342A6Q3.py
@@ -13,8 +13,8 @@ f = lambda x: math.exp(-x**2)
 """
 I got |f"(x)|max=2 and |f""(x)|max=12 and we have b-a =1 and error =0.001.
 After calculating n from the given formula of error for three methods, I got
-n=9.128 for midpoint method so ceil(n)=10, n=12.90 for trapezoidal mrthod so 
-ceil(n)=13 and n=2.85 so ceil(n)=3
+n=9.128 for midpoint method so ceil(n)=10, n=12.90 for trapezoidal method so 
+ceil(n)=13 and n=2.85 so ceil(n)=3 but as it is odd so I have taken n=4 for simpson method
 """
 # results are
 p = lib.midpoint(f, 0, 1, 10)
@@ -23,9 +23,9 @@ print(" result of the integration is ", p)
 p = lib.trapezoidal(f, 0, 1, 13)
 print(" result of the integration is ", p)
  
-p = lib.simpson(f, 0, 1, 3)
+p = lib.simpson(f, 0, 1, 4)
 print(" result of the integration is ", p)
  
 #result of the integration is  0.7471308777479975
 #result of the integration is  0.7464612610366896
-#result of the integration is  0.6921774983654255
+#result of the integration is  0.7468553797909873
