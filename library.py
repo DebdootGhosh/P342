cb5a8fa022ec87e7c681a1c057c493d09ef79a9d
DebdootGhosh <42205626+DebdootGhosh@users.noreply.github.com>
2020-12-06 21:51:55 +0530

    Add files via upload

--- library.py
+++ library.py
@@ -421,4 +421,119 @@ def monte_carlo(f, a, b, n):
     
     return result    
 
+
+# Euler method
+def euler(f,x0,y0,h,xn):
+    
+    # Calculating no. of steps
+    n= int ((xn-x0)/h)
+    
+    # printing the xi and y(xi) data 
+    print('\n-----------SOLUTION-----------')
+    print('------------------------------')    
+    print('x0   \ty0   ')
+    print('------------------------------')
+    for i in range(n+1):
+        slope = f(x0, y0)
+        yn = y0 + h * slope
+        print('%.5f\t%.5f\t'% (x0,y0) )
+        y0 = yn
+        x0 = x0+h
+
+        
+# RK-4 method
+def rk4(f,x0,y0,v0,xn,h):
+    
+    # Calculating no. of steps 
+    n = int((xn-x0)/h)
+    # to solve dy/dx = v 
+    def v(x,v):
+        return v
+    print('\n--------SOLUTION---------------')
+    print('--------------------------------')    
+    print('x0  \ty0  \tyn ')
+    print('--------------------------------')
+    # solving dv/dx = f(x,y,v) and dy/dx=v(x,v) simultaneously
+    for i in range(n+1):
+        k1 = h * (f(x0, y0,v0))
+        k1v = h* (v(x0, v0))
+        k2 = h * (f((x0+h/2), (y0+k1/2),v0))
+        k2v = h* (v((x0+h/2),v0))
+        k3 = h * (f((x0+h/2), (y0+k2/2),v0))
+        k3v = h* (v((x0+h/2),v0))
+        k4 = h * (f((x0+h), (y0+k3),v0))
+        k4v = h* (v((x0+h),v0))
+        k= (k1+2*k2+2*k3+k4)/6
+        kv = (k1v+2*k2v+2*k3v+k4v)/6
+        yn = y0 + kv
+        vn = v0 + k
+        print('%.5f\t%.5f\t%.5f'% (x0,y0,yn) )
+        #print('-----------------------------')
+        y0 = yn
+        x0 = x0+h
+        v0 = vn
+    return yn
+
+#shooting method for boundary value problem
+def shooting_method(f1,f2,x0,y0,xn,yn,h,g):
+     # yn for the first guess
+    y = rk4(f2, x0, y0, g, xn, h)
+    print(f"Value of y(x=xn) for the above guess {g}=",y)
+
+    # initialising
+    lower,upper=0.0,0.0
+    # checking if y overshoots or undershoots
+    # if y overshoots
+    if y > yn and abs(y - yn) > 10E-4:
+        upper = g
+        # we get upper bracket of y
+        # to find lower bound
+        while y > yn:
+            g = float(input(f"Guess a value of y\'({x0}) lower than the previous guess\n"))
+            y =rk4(f2, x0, y0, g, xn, h)
+            print(f"Value of y(x=xn) for the above guess {g}=", y)
+         # if yn for the guess is equal to or very near to actual yn   
+        if abs(y - yn) < 10E-4:
+            y=rk4(f2, x0, y0, g, xn, h)     
+            print(f"Value of y(x=xn) for the above guess {g}=", y)
+            print("Value of y(x=xn) found, integration successful")
+            return 0
+        else:                                # if yn of guess is less than actual yn
+            lower = g                        # then we have found the lower bracket
+            lagrange_interpolation(upper,lower,f1,f2,x0,y0,xn,yn,h)
+
+        # if y undershoots
+    elif y < yn and abs(y - yn) > 10E-4:
+        lower = g     # got the lower bracket
+        # now to find upper bound
+        while y < yn:
+            g = float(input(f"Guess a value of y\'({x0}) greater than the previous guess\n"))
+            y = rk4(f2, x0, y0, g, xn,h)
+            print(f"Value of y(x=xn) for the above guess {g}=", y)
+
+        if abs(y - yn) < 10E-4:
+            rk4(f2, x0, y0, g, xn, h)      
+            print("Value of y(x=xn) found, integration successful")
+        else:
+            upper = g
+            lagrange_interpolation(upper, lower, f1, f2, x0, y0, xn, yn, h)
+        # if yn for the guess is equal to or very near to actual yn
+    elif abs(y - yn) < 10E-4:           
+        y=rk4( f2, x0, y0, g, xn, h)  
+        print(f"Value of y(x=xn) for the above guess {g}=", y)
+        print("Value of y(x=xn) found, integration successful")
+
+
+
+def lagrange_interpolation(upper,lower,f1, f2, x0, y0, xn, yn, h):
+    # yn for lower bracket
+    yl = rk4(f2, x0, y0, lower,xn, h)    
+    # yn for upper bracket
+    yh = rk4(f2, x0, y0, upper, xn, h)    
+    # for next y'(x0)
+    g = lower + ((upper - lower) / (yh - yl)) * (yn - yl)
+    # yn for the new y'(x0)
+    y = rk4(f2, x0, y0, g, xn, h)
+    print("Value of y(x=xn) found, integration successful")
+    
         
